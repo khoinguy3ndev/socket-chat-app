0dd8c8faabaf3d6426eefe7e2887655a9d2ab405
Nguyen Khoi <khoibene@gmail.com>
2025-12-04 23:42:02 +0700

    import path

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 app.use(cors({
-  origin: 'http://localhost:5500',
+  origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
